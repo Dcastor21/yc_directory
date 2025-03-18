b040949b018128dcbcb9eb6cb47f9f1ab337ea22
Darnel Castor <darnelcastor2@gmail.com>
2025-03-18 17:48:59 -0400

    Add author schema type to Sanity Studio

---
+++ sanity/schemaTypes/author.ts
@@ -0,0 +1,13 @@
+import { defineField, defineType } from "sanity";
+
+export const author = defineType({
+  name: "author",
+  title: "Author",
+  type: "document",
+  fields: [
+    defineField({
+      name: "id",
+      type: "number",
+    }),
+  ],
+});
